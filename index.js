f8d57774b83ef8f42d7109aa975e0d0775177d38
Ruslan <ruslan.akopyan@ukr.net>
2021-12-20 22:51:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const openPopUp = document.getElementById("open-pop-up");
+const closePopUp = document.getElementById("pop-up-close");
+const popUp = document.getElementById("pop-up");
+
+
+openPopUp.addEventListener('click', function (e){
+    e.preventDefault();
+    popUp.classList.add('active');
+});
+
+closePopUp.addEventListener('click', () => {
+    popUp.classList.remove('active');
+});
